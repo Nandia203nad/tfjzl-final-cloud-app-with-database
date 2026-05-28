1910073c8b957e9145781a98f9dae539bba53d31
Nandia203nad <nadyanandia203@gmail.com>
2026-05-28 18:02:52 +0800

    Import Submission model and update admin inlines
    Added Submission model to admin registration and updated inlines.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,41 +1,37 @@
 from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Question, Choice
+# Шаардлагатай 7 моделийг бүгдийг нь импортлох (Submission нэмэгдсэн)
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
 
-# Inlines for Admin
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
-class QuestionInline(admin.StackedInline):
-    model = Question
-
-
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-
-
-# Admin Format
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline, QuestionInline]
+    inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'grade')
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-
-
-# Register Models to Admin
+# Бүх 7 моделийг админ сайт руу бүртгэх (заавал Submission-ийг бүртгээрэй)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Submission)  
